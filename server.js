f4c1bbabdc86af4f818ecbc584b44f716cd98a8b
Jensey <50929863+seyleigh@users.noreply.github.com>
2020-01-09 20:19:29 -0500

    changed files structure?

--- server.js
+++ server.js
@@ -2,31 +2,18 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const mongoose = require("mongoose")
 
-
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/gbooksdb";
-
-mongoose.connect(MONGODB_URI, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true
-  });
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
-
-// Send every other request to the React app
+// Send every request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
+app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
