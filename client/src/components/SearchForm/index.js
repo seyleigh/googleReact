559cb11c067143c7d18f6c2f5070621013472758
Jensey <50929863+seyleigh@users.noreply.github.com>
2020-01-13 20:08:16 -0500

    stupid search form

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -5,9 +5,9 @@ const SearchForm = props => {
     return (
         <form>
             <div className="form-group">
-                <label className="BookSearchLabel text-center"><h3>Search for Books</h3></label>
+                <label className="BookSearchLabel text-center"><h1>Search for Books</h1></label>
                 <br />
-                <input className="col-12 form-control" value={props.search} type="text" name="searchBook" placeholder="Type your book here" onChange={props.handleInputChange} />
+                <input className="col-12 form-control" value={props.search} type="text" name="searchBook" required placeholder="Type your book here" onChange={props.handleInputChange} />
             </div>
             <button type="submit" className="submitBtn btn btn-outline-light" onClick={props.handleFormSubmit}><i className="fas fa-check"></i> Get books</button>
         </form>
