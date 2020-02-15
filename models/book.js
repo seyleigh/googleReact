116f7d926b14f06feae90dd64ad94bcac6bba921
Jensey <50929863+seyleigh@users.noreply.github.com>
2020-02-14 20:50:55 -0500

    deleted unnecessary punctuation

--- models/book.js
+++ models/book.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const bookSchema = new Schema({
     title: { type: String, required: true },
-    authors: [{ type: String, required: true }],
+    author: [{ type: String, required: true }],
     description: String,
     image:  { type: String, trim: true },
     link: { type: String, trim: true },
